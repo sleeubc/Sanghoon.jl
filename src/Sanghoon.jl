362b4796f193ee63722c5ba5f5a6166e730aadb3
Sanghoon Lee <sanghoon.lee@sauder.ubc.ca>
2022-06-23 13:39:07 -0700

    Add subtypetree

--- src/Sanghoon.jl
+++ src/Sanghoon.jl
@@ -2,10 +2,16 @@ module Sanghoon
 
 # Write your package code here.
 
-export test
+export subtypetree
 
-function test()
-    println("Hello Keith!")
+# Display the entire type hierarchy starting from the specified `roottype`
+# Source: Tom Kwong, Hands-on design patterns and best practices with Julia
+function subtypetree(roottype, level = 1, indent = 4)
+    level == 1 && println(roottype)
+    for s in subtypes(roottype)
+    println(join(fill(" ", level * indent)) * string(s))
+    subtypetree(s, level + 1, indent)
+    end
 end
 
 end
